fb61c427631441f7a7aa9b5b353638800d6dd1af
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-12 02:11:46 -0500

    Add NavBar

---
+++ client/src/components/modules/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./NavBar.css";
+import "../../utilities.css";
+
+
+const NavBar = () => {
+  return (
+    <nav className="NavBar-container">
+      <div className="u-flex">[logo]</div>
+      <div className="NavBar-linkContainer u-flex">
+        <NavLink to="/" className="NavBar-link">Home</NavLink>
+        <NavLink to="/report/" className="NavBar-link">Report closed route</NavLink>
+        <NavLink to="/settings/" className="NavBar-link">Settings</NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
