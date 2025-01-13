271fed8e6c33b684790205871d66ea621c4ab57d
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-13 11:53:00 -0500

    Revert "move login button to NavBar"
    This reverts commit dc4bfcd9f17b2ae81e82468548aa098e5e5c331d.

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -1,42 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import "./NavBar.css";
 import "../../utilities.css";
-import { UserContext } from "../App";
+
 
 const NavBar = () => {
-  const { userId, handleLogin, handleLogout } = useContext(UserContext);
   return (
     <nav className="NavBar-container">
       <div className="u-flex">[logo]</div>
-      <div className="u-flex u-flex-spaceBetween">
-        <div className="NavBar-linkContainer u-flex u-flex-gap-s">
-          <NavLink to="/" className="NavBar-link">
-            Home
-          </NavLink>
-          <NavLink to="/report/" className="NavBar-link">
-            Report closed route
-          </NavLink>
-          <NavLink to="/settings/" className="NavBar-link">
-            Settings
-          </NavLink>
-        </div>
-        <div className="u-flex">
-          {userId ? (
-            <button
-              onClick={() => {
-                googleLogout();
-                handleLogout();
-              }}
-            >
-              Logout
-            </button>
-          ) : (
-            <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-          )}
-        </div>
+      <div className="NavBar-linkContainer u-flex">
+        <NavLink to="/" className="NavBar-link">Home</NavLink>
+        <NavLink to="/report/" className="NavBar-link">Report closed route</NavLink>
+        <NavLink to="/settings/" className="NavBar-link">Settings</NavLink>
       </div>
     </nav>
   );
