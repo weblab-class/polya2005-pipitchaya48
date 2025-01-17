8d1fab0d666590fa6030fb7426c949657dec24b1
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-17 18:07:46 -0500

    store the entire user as context

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,13 +17,13 @@ export const UserContext = createContext(null);
  * Define the "App" component
  */
 const App = () => {
-  const [userId, setUserId] = useState(undefined);
+  const [user, setUser] = useState(undefined);
 
   useEffect(() => {
     get("/api/whoami").then((user) => {
       if (user._id) {
         // they are registed in the database, and currently logged in.
-        setUserId(user._id);
+        setUser(user);
       }
     });
   }, []);
@@ -33,18 +33,18 @@ const App = () => {
     const decodedCredential = jwt_decode(userToken);
     console.log(`Logged in as ${decodedCredential.name}`);
     post("/api/login", { token: userToken }).then((user) => {
-      setUserId(user._id);
+      setUser(user);
       post("/api/initsocket", { socketid: socket.id });
     });
   };
 
   const handleLogout = () => {
-    setUserId(undefined);
+    setUser(undefined);
     post("/api/logout");
   };
 
   const authContextValue = {
-    userId,
+    user,
     handleLogin,
     handleLogout,
   };
