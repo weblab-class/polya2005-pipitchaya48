a67af302904f54e00313ceddc1896f697978b97a
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-17 23:58:01 -0500

    add switch menu and handle non-array children

--- client/src/components/modules/MenuList.jsx
+++ client/src/components/modules/MenuList.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ChevronRight } from "@mui/icons-material";
+import { Switch } from "./Switch";
 
 export const MenuList = ({ children }) => {
+  if (!Array.isArray(children)) {
+    return <div className="flex flex-col w-screen p-s">{children}</div>;
+  }
+
   return (
     <div className="flex flex-col w-screen p-s">
       {children.map((child, index) => (
-        <>
+        <React.Fragment key={index}>
           {index === 0 || <hr />} {child}
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
@@ -26,3 +31,12 @@ export const MenuListItem = ({ children, onClick, className }) => {
     </div>
   );
 };
+
+export const MenuListSwitch = ({ children, onChange, className }) => {
+  return (
+    <div className="flex items-center justify-between p-s">
+      <div className={className}>{children}</div>
+      <Switch onChange={onChange} />
+    </div>
+  )
+}
