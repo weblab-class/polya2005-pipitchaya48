49ad039bb3e77861aa49c6f72ed5c6729677387b
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-19 02:22:57 -0500

    use await

--- client/src/components/pages/Results.jsx
+++ client/src/components/pages/Results.jsx
@@ -11,37 +11,47 @@ const RouteUpdater = ({ route }) => {
   const numCoords = route.length;
   const coordinatesPromise = (locationId) =>
     get("/api/location-coords", { locationId: locationId });
-  Promise.all(route.slice(1, numCoords - 1).map(coordinatesPromise)).then((coords) => {
-    coordinatesPromise(route[0]).then((startCoords) => {
-      coordinatesPromise(route[numCoords - 1]).then((endCoords) => {
-        coords = [startCoords, ...coords, endCoords].map((coord) => coordinatesToArray(coord));
-        coords.map((coord, index) => {
-          let marker;
-          switch (index) {
-            case 0:
-              marker = circleMarker(coord, {
-                radius: 10,
-                color: "#750014",
-              });
-              break;
-            case numCoords - 1:
-              marker = mapMarker(coord, { color: "#8b959e" });
-              break;
-            default:
-              marker = circleMarker(coord, {
-                radius: 5,
-                stroke: false,
-                fillOpacity: 1,
-                color: "#8b959e",
-              });
-              break;
-          }
-          map.addLayer(marker);
-        });
-        map.addLayer(polyline(coords, { color: "#8b959e" }));
-      });
+  const fetchCoordinates = async (locationId) => {
+    const coords = await get("/api/location-coords", { locationId: locationId });
+    return coordinatesToArray(coords);
+  };
+
+  const updateRoute = async () => {
+    const startCoords = await fetchCoordinates(route[0]);
+    const endCoords = await fetchCoordinates(route[numCoords - 1]);
+    const middleCoords = await Promise.all(route.slice(1, numCoords - 1).map(fetchCoordinates));
+    const coords = [startCoords, ...middleCoords, endCoords];
+
+    coords.forEach((coord, index) => {
+      let marker;
+      switch (index) {
+        case 0:
+          marker = circleMarker(coord, {
+            radius: 10,
+            color: "#750014",
+          });
+          break;
+        case numCoords - 1:
+          marker = mapMarker(coord, { color: "#8b959e" });
+          break;
+        default:
+          marker = circleMarker(coord, {
+            radius: 5,
+            stroke: false,
+            fillOpacity: 1,
+            color: "#8b959e",
+          });
+          break;
+      }
+      map.addLayer(marker);
     });
-  });
+
+    map.addLayer(polyline(coords, { color: "#8b959e" }));
+  };
+
+  useEffect(() => {
+    updateRoute();
+  }, [route]);
   return null;
 };
 
