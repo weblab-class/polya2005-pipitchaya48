41bd0948b9db8949fffa69021dbab5cd4fee39e8
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-18 23:13:34 -0500

    use history.back

--- client/src/components/modules/SettingsHeading.jsx
+++ client/src/components/modules/SettingsHeading.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 export const SettingsHeading = ({children}) => {
   return <div className="flex items-center w-full px-4 py-2">
-    <Link to="/settings">
+    <div onClick={() => window.history.back()} className="cursor-pointer">
       <ChevronLeft />
-    </Link>
+    </div>
     <h1 className="text-2xl text-center w-full">{children}</h1>
     <div className="w-6"></div>
   </div>;
