144909f72a5abae59395b1e22c8916383ac2051e
polya2005 <65003790+polya2005@users.noreply.github.com>
2025-01-28 21:38:41 -0500

    move images into the img folder

--- client/src/components/modules/NavBar.jsx
+++ client/src/components/modules/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { GoogleLogin } from "@react-oauth/google";
 import { UserContext } from "../App";
@@ -7,12 +7,15 @@ import "./NavBar.css";
 import "../../utilities.css";
 
 const NavBar = () => {
-  const {user, handleLogin} = useContext(UserContext);
+  const { user, handleLogin } = useContext(UserContext);
   return (
     <nav className="flex flex-col w-full bg-mit-red p-m justify-around shadow">
       <div className="flex justify-between">
-        <img src="../../../mit_logo.svg" className="h-4 my-2 self-center"/>
-        {user?null:<GoogleLogin onSuccess={handleLogin} type="icon" size="medium"/>}
+        <div className="flex gap-s content-center">
+          <img src="../../../img/mit_logo.svg" className="h-4 my-2 self-center" />
+          <h2 className="text-white text-l font-bold align-middle my-1">Tunnel Navigator</h2>
+        </div>
+        {user ? null : <GoogleLogin onSuccess={handleLogin} type="icon" size="medium" />}
       </div>
       <div className="px-0 py-s flex gap-s">
         <NavLink to="/" className="text-white text no-underline p-s rounded-lg align-middle">
